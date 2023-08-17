d842f00682820f33c9d626fd6f81e7bf765e5cf1
Eungyeong Lee <anhanmu78@gmail.com>
2023-08-17 14:20:09 +0900

    modified slurm directory

--- slurm/03_conda.py
+++
@@ -1,6 +0,0 @@
-import torch
-import socket
-
-print("host name: ",socket.gethostname())
-print("Using GPU is " + str(torch.cuda.is_available()))
-print("Using " + str(torch.cuda.device_count()) + " GPUs")
